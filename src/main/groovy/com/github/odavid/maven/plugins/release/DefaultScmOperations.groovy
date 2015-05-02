a3302af5ad76929f33c6ef919a6b6a7d39212bee
odavid <ohad.david@gmail.com>
2015-05-02 21:00:39 +0300

    Starting scm commit phase

--- src/main/groovy/com/github/odavid/maven/plugins/release/DefaultScmOperations.groovy
+++ src/main/groovy/com/github/odavid/maven/plugins/release/DefaultScmOperations.groovy
@@ -1,22 +1,26 @@
 package com.github.odavid.maven.plugins.release
 
 import org.apache.maven.scm.ScmFileSet
-import org.apache.maven.scm.provider.ScmProvider
+import org.apache.maven.scm.ScmTagParameters
+import org.apache.maven.scm.manager.ScmManager
 import org.apache.maven.scm.repository.ScmRepository
 
 
 class DefaultScmOperations implements ScmOperations{
 	ScmRepository scmRepository
-	ScmProvider scmProvider
+	ScmManager scmManager
 
 	@Override
 	public void commitReleaseInfo(File releaseInfoFile, String commitMessage) {
 		ScmFileSet scmFileSet = new ScmFileSet(releaseInfoFile.parentFile, releaseInfoFile)
-		scmProvider.checkIn(scmRepository, scmFileSet, commitMessage)
+		scmManager.checkIn(scmRepository, scmFileSet, commitMessage)
 	}
 
 	@Override
 	public void tagWorkspace(File basedir, String tagName) {
+		ScmFileSet scmFileSet = new ScmFileSet(basedir)
+		ScmTagParameters params = new ScmTagParameters("Tagging release: ${tagName}")
+		scmManager.tag(scmRepository, scmFileSet, tagName, params)
 	}
 
 }
--- src/main/groovy/com/github/odavid/maven/plugins/release/ReleasePrepareMojo.groovy
+++ src/main/groovy/com/github/odavid/maven/plugins/release/ReleasePrepareMojo.groovy
@@ -12,6 +12,8 @@ import org.apache.maven.plugins.annotations.Mojo
 import org.apache.maven.plugins.annotations.Parameter
 import org.apache.maven.plugins.annotations.ResolutionScope
 import org.apache.maven.project.MavenProject
+import org.apache.maven.scm.manager.ScmManager
+import org.apache.maven.scm.repository.ScmRepository
 import org.eclipse.aether.RepositorySystem
 
 import com.github.odavid.maven.plugins.groovy.GroovyMojoWithAntLogger
@@ -21,6 +23,13 @@ import com.github.odavid.maven.plugins.groovy.GroovyMojoWithAntLogger
  */
 @Mojo(name="prepare", requiresDependencyResolution=ResolutionScope.NONE, requiresProject=true, threadSafe=true, aggregator=true)
 class ReleasePrepareMojo extends GroovyMojoWithAntLogger{
+	@Parameter(defaultValue='false')
+	boolean dryRun
+
+	@Parameter(required=true, property='buildNumber')
+	Long buildNumber;
+
+
 	@Parameter(required=true, readonly=true, defaultValue='${basedir}')
 	File basedir
 	
@@ -33,14 +42,14 @@ class ReleasePrepareMojo extends GroovyMojoWithAntLogger{
 	@Parameter(readonly=true, defaultValue='${session}')
 	MavenSession session
 	
-	@Parameter(required=true, property='buildNumber')
-	Long buildNumber;
-
 	@Parameter(required=true, property='releaseInfoFile', defaultValue='${basedir}/release-info.json')
 	File releaseInfoFile;
 
 	@Inject
 	RepositorySystem repositorySystem
+	
+	@Inject
+	ScmManager scmManager
 	
 	
 	@Override
@@ -48,7 +57,7 @@ class ReleasePrepareMojo extends GroovyMojoWithAntLogger{
 		DefaultRepositoryMetadataService repositoryMetadataService = new DefaultRepositoryMetadataService(repositorySystem, session.repositorySession, RepositoryUtils.toRepos( project.remoteArtifactRepositories), log)
 		DefaultReleaseContext context = new DefaultReleaseContext(repositoryMetadataService, session, project, reactorProjects, buildNumber, log)
 		
-		log.info("Preparing release, buildNumber: ${buildNumber}, reactor: ${project.id}")
+		log.info("Preparing release, buildNumber: ${buildNumber}, reactor: ${project.id}, dryRun = ${dryRun}")
 		log.info("Analyzing ${reactorProjects.size()} reactor modules")
 		List<ReleasableModule> projects = context.analyzeReactor()
 		List<ReleaseModuleProblem> problems = context.validate(projects)
@@ -76,13 +85,32 @@ class ReleasePrepareMojo extends GroovyMojoWithAntLogger{
 		if(releaseInfoFile.exists()){ releaseInfoFile.delete() }
 		releaseInfoFile.withWriter{w -> w << json.toPrettyString() } 
 		log.info("release-info.json was written")
-		
-		log.info("Changing pom files")
-		PomManipulator pomManipulator = new PomManipulator()
-		projects.each {
-			log.info("Changing ${it.file}")
-			pomManipulator.updatePom(it, basedir, log)
+
+		if(!dryRun){
+			log.info("Getting scm url")
+			String scmUrl = project?.scm.developerConnection
+			log.info("Got scm url: ${scmUrl}. Initializing scm repository")
+			ScmOperations scmOperations = createScmOperations(scmUrl)
+			log.info("Committing release info file")
+			scmOperations.commitReleaseInfo(releaseInfoFile, "releas-info")
 		}
-		log.info("pom files were changed")
+// 		SHOULD BE USED In perform stage				
+//		log.info("Changing pom files")
+//		PomManipulator pomManipulator = new PomManipulator()
+//		projects.each {
+//			log.info("Changing ${it.file}")
+//			pomManipulator.updatePom(it, basedir, log)
+//		}
+//		log.info("pom files were changed")
+	}
+	String getScmUrl(MavenProject project){
+		String scmUrl = project?.scm.developerConnection
+		if(!scmUrl){
+			throw new IllegalStateException("No SCM URL is defined")
+		}
+	}
+	ScmOperations createScmOperations(String scmUrl){
+		ScmRepository repository = scmManager.makeScmRepository(scmUrl)
+		ScmOperations operations = new DefaultScmOperations(scmRepository: repository)
 	}
 }
